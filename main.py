ac4c5c8d4d77f88deff253f7f9652238b38837a0
Luís Marques <l.marques@sap.com>
2024-05-27 18:20:48 +0100

    Implement and test functions to use OpenAI API

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import openai
 import tiktoken
 from dotenv import find_dotenv, load_dotenv
 
-_ = load_dotenv(find_dotenv()) # read local .env file
+_ = load_dotenv(find_dotenv())  # read local .env file
 
-openai.api_key  = os.environ['OPENAI_API_KEY']
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 print(openai.api_key)
-
